348bef2b75b02a67ec7b6b0cf8c7f8fdd51fa0d0
Cider-Jude <jude.aybalen@gmail.com>
2025-11-27 10:09:10 +0100

    Start of the project repository: data selected and start of EDA

---
+++ Code/import csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv("Most Streamed Spotify Songs 2024.csv", encoding="latin-1")
+
+# Convert the numbers higher than 1000 from strings 
+for col in df.columns:
+    if df[col].dtype == "object" and col not in ["Track", "Album Name", "Artist"]:
+        df[col] = df[col].astype(str).str.replace(",", "")
+        numeric_series = pd.to_numeric(df[col], errors="coerce")
+
+        # If at least one value converted successfully, replace column
+        if numeric_series.notna().any():
+            df[col] = numeric_series
+
+print(df.head())
+
+
+for col in df.columns:
+    if df[col].dtype == "float64":
+        range = df[col].max() - df[col].min()
+        print(f"\n{col} \n- Mean: {df[col].mean()} \n- Median: {df[col].median()} \n- Standard deviation: {df[col].std()} \n- Variance: {df[col].var()} \n- Maximum: {df[col].max()} \n- Minimum: {df[col].min()} \n- Range: {range}")
+
